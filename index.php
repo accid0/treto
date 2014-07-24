da7a5d1d2b5275b80e92890c264756a11e40a1fe
reHePaJI <kontakt.asch@gmail.com>
2014-07-24 20:36:53 +0300

    fixed

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ class File {
         ]);
       $url                            = preg_replace('@^https?://(?:www\.)?@i', '', $url);
       $url                            = str_ireplace('treto.ru', '91.228.154.101', $url);
-      $raw                            = file_get_contents($url);
+      $raw                            = file_get_contents('http://' . $url);
       echo strlen($raw), PHP_EOL;
       if(empty($raw)){
         continue;
